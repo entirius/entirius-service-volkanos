b44b132f7899f123d074bc3b229aef5544f37d4f
kloczi <michalk@lazelab.com>
2026-09-13 14:52:28 +0200

    Guarded urls include for django_siteintel (leads-platform pre-condition for plan 04)

--- main/urls.py
+++ main/urls.py
@@ -78,6 +78,8 @@ if "django_contact_forms" in settings.INSTALLED_APPS:
     urlpatterns.append(path("", include("django_contact_forms.urls")))
 if "django_notifications" in settings.INSTALLED_APPS:
     urlpatterns.append(path("", include("django_notifications.urls")))
+if "django_siteintel" in settings.INSTALLED_APPS:
+    urlpatterns.append(path("", include("django_siteintel.urls")))
 if "django_regon_api" in settings.INSTALLED_APPS:
     urlpatterns.append(path("", include("django_regon_api.urls")))
 if "django_atlas" in settings.INSTALLED_APPS:
